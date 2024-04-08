226550693ac9a091f108ac7776cb7f7775f9cf6b
StefDev1801 <stefano200118@gmail.com>
2024-04-08 21:39:49 +0200

    first commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,10 @@
+<script>
+    export default {
+        name: 'AppHeader'
+    }
+</script>
+<template>
+    <h1>Header</h1>
+    <img src="/public/images/HelloIMG1669149314369.png" alt="">
+</template>
+<style></style>
