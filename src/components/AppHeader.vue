79804e07cf8c2405439fcb22c1f7ced23f46235e
StefDev1801 <stefano200118@gmail.com>
2024-04-09 12:15:28 +0200

    add draft navigation bar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,7 +4,40 @@
     }
 </script>
 <template>
-    <h1>Header</h1>
-    <img src="/public/images/HelloIMG1669149314369.png" alt="">
+    <div class="header_nav_bar">
+        <div>
+            <img src="/public/images/HelloIMG1669149314369.png" alt="">
+        </div>
+        <ul>
+            <li>
+                HOME
+            </li>
+            <li>
+                MISSION
+            </li>
+            <li>
+                CAUSES
+            </li>
+            <li>
+                JOURNAL
+            </li>
+            <li>
+                <button>DONATE</button>
+            </li>
+        </ul>
+    </div>
+    
 </template>
-<style></style>
+<style>
+ul{
+    list-style: none;
+    display: flex;
+}
+li{
+    padding: 1rem;
+}
+.header_nav_bar{
+    display: flex;
+    justify-content: space-between;
+}
+</style>
